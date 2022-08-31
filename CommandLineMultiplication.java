473b3b905842cd0fb81a420dfa2f6bd747177cd7
ksschemmel <112411813+ksschemmel@users.noreply.github.com>
2022-08-31 11:48:26 -0400

    Add files via upload

---
+++ CommandLineMultiplication.java
@@ -0,0 +1,12 @@
+public class CommandLineMultiplication
+{
+public static void main(String args[])
+{
+double a = Double.parseDouble(args[0]);
+double b = Double.parseDouble(args[1]);
+double c = Double.parseDouble(args[2]);
+
+System.out.println("Multiplication:"+(a*b*c));
+}
+
+}
